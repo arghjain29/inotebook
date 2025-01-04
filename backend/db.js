c37052a29d34c4d8ff7b15ba5cf7fde484b8b6da
arghjain29 <arghjain1234@gmail.com>
2025-01-04 22:36:05 +0530

    add.

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017/inotebook"
-const mongoURI = "mongodb+srv://arghjain1234:<db_password>@inotebook.3oe1f.mongodb.net/?retryWrites=true&w=majority&appName=Inotebook"
+const mongoURI = "mongodb+srv://arghjain1234:arghjain2004@inotebook.3oe1f.mongodb.net/?retryWrites=true&w=majority&appName=Inotebook"
 
 const connectToMongo = async () => {
   mongoose.connect(mongoURI).then(() => console.log("Connected")).catch((e) => console.log(e.message));
